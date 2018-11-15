06b61413c614c82ab532ccba72d556621e294647
serjcooper <ewqP[539710!>
2018-11-15 15:38:11 +0300

    Добавил метод чтение конфигов в формате json (сырой) Добавил тест на открытие всего каталога Добавил заготовку для сбора списка всех подкаталогов. Есть баг, что тесты отрабатывают непоследовательно, логин происходит после проверки каталога

--- src/test/java/Autotests/FirstTest.java
+++ src/test/java/Autotests/FirstTest.java
@@ -1,33 +1,36 @@
 package Autotests;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FirstTest {
 
     private static WebDriver driver;
 
     @BeforeClass
     public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
-        driver = new ChromeDriver();
+        initConfig configs = new initConfig();
+        System.setProperty("webdriver." + configs.browserName +".driver", configs.driverPath);
+        if ("Chrome".equalsIgnoreCase(configs.browserName)) {             //заготовка под выбор браузеров
+            driver = new ChromeDriver();
+        }
         driver.manage().window().maximize(); //передаю веб-драйверу набор методов, для того чтобы ход теста отображался в полностью открытом окне:
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // неявное ожидание Implicit Wait, которое задается вначале теста и будет работать при каждом вызове метода поиска элемента:
-        driver.get("https://shop.by/");
+        driver.get(configs.targetUrl);
     }
     @Test
     public void userLogin() {
         WebElement authWindow = driver.findElement(By.xpath("//span[@class='chzn-txt-sel'][contains(text(),'Войти')]"));
         authWindow.click();
         WebElement loginField = driver.findElement(By.cssSelector("#LLoginForm_phone"));
+        loginField.click();
         loginField.sendKeys("447659060");
         WebElement passwordField = driver.findElement(By.cssSelector("#LLoginForm_password"));
         passwordField.sendKeys("testa1qa");
@@ -36,9 +39,20 @@ public class FirstTest {
         WebElement profileUser = driver.findElement(By.xpath("//div[@class='Header__BlockNameUser']"));
         String UserName = profileUser.getText();
         Assert.assertEquals("userShop.by_20", UserName);
-
-
     }
+
+    @Test
+    public void openSummaryCatalog() {
+        WebElement summaryCatalogButton = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/a"));
+        summaryCatalogButton.click();
+        Assert.assertEquals("Весь каталог", driver.findElement(By.className("Page__TitleActivePage")).getText());
+    }
+
+    @Test
+    public void openSameCatalog() {
+        List<WebElement> catalogList = driver.findElements(By.className("Page__SectionText"));
+    }
+
     @AfterClass
     public static void tearDown() {
         WebElement menuUser = driver.findElement(By.xpath("//a[@class='Header__BlockLinkOffice Header__LinkPersonalCabinet Page__SelectOnBg Header__LinkShowWapper']//div[@class='chzn-btn glyphicon']"));
